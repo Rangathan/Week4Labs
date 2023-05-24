66f32db325496b76ded8a36ffbb618d3e20173f1
Rangathan <121978255+Rangathan@users.noreply.github.com>
2023-05-24 12:08:52 -0700

    Add files via upload

---
+++ lab4/Form1.cs
@@ -0,0 +1,25 @@
+namespace lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
